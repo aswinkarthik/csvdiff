fe10dfa150e1eaa5adfc60a089f429e10e831046
Aswin Karthik <aswinkarthik93@gmail.com>
2019-10-08 18:41:26 +0530

    Validation positions to prevent out of bounds panic

--- cmd/root.go
+++ cmd/root.go
@@ -102,7 +102,6 @@ func runContext(
 	return NewFormatter(outputStream, errorStream, ctx).Format(diff)
 }
 
-
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
