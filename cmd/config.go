a61bbad5455375eca18fbcfda7e9b71cf0c94b32
Aswin Karthik <aswinkarthik93@gmail.com>
2019-10-08 17:34:48 +0530

    Introduce afero.Fs to do basefile and deltafile validation

--- cmd/config.go
+++ cmd/config.go
@@ -1,7 +1,8 @@
 package cmd
 
 import (
-	"errors"
+	"fmt"
+	"github.com/spf13/afero"
 	"strings"
 
 	"github.com/aswinkarthik/csvdiff/pkg/digest"
@@ -13,6 +14,8 @@ type Config struct {
 	ValueColumnPositions   []int
 	IncludeColumnPositions []int
 	Format                 string
+	BaseFilename           string
+	DeltaFilename          string
 }
 
 // GetPrimaryKeys is to return the --primary-key flags as digest.Positions array.
@@ -42,13 +45,52 @@ func (c Config) GetIncludeColumnPositions() digest.Positions {
 
 // Validate validates the config object
 // and returns error if not valid.
-func (c *Config) Validate() error {
+func (c *Config) Validate(fs afero.Fs) error {
+	{
+		// format validation
 
-	for _, format := range allFormats {
-		if strings.ToLower(c.Format) == format {
-			return nil
+		formatFound := false
+		for _, format := range allFormats {
+			if strings.ToLower(c.Format) == format {
+				formatFound = true
+			}
+		}
+		if !formatFound {
+			return fmt.Errorf("specified format is not valid")
 		}
 	}
 
-	return errors.New("Specified format is not valid")
+	{
+		// base-file validation
+
+		if exists, err := afero.Exists(fs, c.BaseFilename); err != nil {
+			return fmt.Errorf("error reading base-file %s: %v", c.BaseFilename, err)
+		} else if !exists {
+			return fmt.Errorf("base-file %s does not exits", c.BaseFilename)
+		}
+
+		if isDir, err := afero.IsDir(fs, c.BaseFilename); err != nil {
+			return fmt.Errorf("error reading base-file %s: %v", c.BaseFilename, err)
+		} else if isDir {
+			return fmt.Errorf("base-file %s should be a file", c.BaseFilename)
+		}
+	}
+
+	{
+		// delta file validation
+
+		if exists, err := afero.Exists(fs, c.DeltaFilename); err != nil {
+			return fmt.Errorf("error reading delta-file %s: %v", c.DeltaFilename, err)
+		} else if !exists {
+			return fmt.Errorf("delta-file %s does not exits", c.DeltaFilename)
+		}
+
+		if isDir, err := afero.IsDir(fs, c.DeltaFilename); err != nil {
+			return fmt.Errorf("error reading delta-file %s: %v", c.DeltaFilename, err)
+		} else if isDir {
+			return fmt.Errorf("delta-file %s should be a file", c.DeltaFilename)
+		}
+	}
+
+	return nil
 }
