e123daca46e99d63828b8bee407c9e9ab7fb72bc
Kyle Dunn <kdunn926@gmail.com>
2020-01-03 13:28:45 -0700

    Exposing lazy quote parameter to allow support for unescaped quotes in data

--- pkg/digest/diff_test.go
+++ pkg/digest/diff_test.go
@@ -24,21 +24,70 @@ func TestDiff(t *testing.T) {
 
 	t.Run("default config", func(t *testing.T) {
 		baseConfig := &digest.Config{
-			Reader:    strings.NewReader(base),
-			Key:       []int{0},
-			Separator: ',',
+			Reader:     strings.NewReader(base),
+			Key:        []int{0},
+			Separator:  ',',
+			LazyQuotes: false,
 		}
 
 		deltaConfig := &digest.Config{
-			Reader:    strings.NewReader(delta),
-			Key:       []int{0},
-			Separator: ',',
+			Reader:     strings.NewReader(delta),
+			Key:        []int{0},
+			Separator:  ',',
+			LazyQuotes: false,
 		}
 
 		expected := digest.Differences{
 			Additions: []digest.Addition{
 				strings.Split("4,col-1,col-2,col-3,four-value-added", ","),
 				strings.Split("5,col-1,col-2,col-3,five-value-added", ","),
+			},
+			Modifications: []digest.Modification{
+				{
+					Current:  strings.Split("2,col-1,col-2,col-3,two-value-modified", ","),
+					Original: strings.Split("2,col-1,col-2,col-3,two-value", ","),
+				},
+				{
+					Current:  strings.Split("100,col-1-modified,col-2,col-3,hundred-value-modified", ","),
+					Original: strings.Split("100,col-1,col-2,col-3,hundred-value", ","),
+				},
+			},
+			Deletions: []digest.Deletion{
+				strings.Split("3,col-1,col-2,col-3,three-value", ","),
+			},
+		}
+
+		actual, err := digest.Diff(*baseConfig, *deltaConfig)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, actual)
+	})
+
+	deltaLazyQuotes := `1,col-1,col-2,col-3,one-value
+2,col-1,col-2,col-3,two-value-modified
+4,col-1,col-2,col-3,four"-added
+100,col-1-modified,col-2,col-3,hundred-value-modified
+5,col-1,col-2,col-3,five"-added
+`
+
+	t.Run("lazy quotes in delta config", func(t *testing.T) {
+		baseConfig := &digest.Config{
+			Reader:     strings.NewReader(base),
+			Key:        []int{0},
+			Separator:  ',',
+			LazyQuotes: false,
+		}
+
+		deltaConfig := &digest.Config{
+			Reader:     strings.NewReader(deltaLazyQuotes),
+			Key:        []int{0},
+			Separator:  ',',
+			LazyQuotes: true,
+		}
+
+		expected := digest.Differences{
+			Additions: []digest.Addition{
+				strings.Split("4,col-1,col-2,col-3,four\"-added", ","),
+				strings.Split("5,col-1,col-2,col-3,five\"-added", ","),
 			},
 			Modifications: []digest.Modification{
 				{
